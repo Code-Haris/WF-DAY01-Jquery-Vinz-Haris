80024119696aa2605df25b8d591640572dab2fff
Vinzenz Grissemann <vinzig@ymail.com>
2019-10-21 15:55:54 +0200

    finished exercise

--- Basic/myscript.js
+++ Basic/myscript.js
@@ -6,38 +6,46 @@
  */
 
 // all elements
-$("*").css("background-color", "yellow");
-
+//$("*").css("background-color","yellow");
 // all p elements
-$("p").css("background-color", "blue");
 
-
+//$("p").css("background-color","yellow");
 
 // the p element with an ID of "intro"
-$("#intro").css("background-color", "red");
 
+//$("p#intro").css("background-color","yellow");
 
 // all elements with a class of "note"
-$(".note").css("background-color", "red");
 
+//$("p.note").css("background-color","yellow");
 
 // all DIV elements with a class of note
-$("div").css("background-color", "red");
 
+//$("div.note").css("background-color","yellow");
 
 // all p elements in the article element
 
+//$("article p").css("background-color","yellow");
 
 // the last element in any section which is a p tag
 
+//$("p:last-of-type").css("background-color","yellow");
 
 // the first paragraph on the page
+
+//$("p:first").css("background-color","yellow");
 
 
 // all p elements which have a link
 
+//$("p:has(a)").css("background-color","yellow");
 
 // the second li within nested ul element
 
+//$("ul ul li:eq(1)").css("background-color","yellow");
 
 // every second and third p element
+
+$("p:nth-of-type(2)").css("background-color","yellow");
+$("p:nth-of-type(3)").css("background-color","yellow");
+
